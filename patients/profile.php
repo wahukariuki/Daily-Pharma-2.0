adfbbbdfecf56835fbb43f2281232fedaffb01f4
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2023-09-03 20:35:13 +0300

    Refactoring patient files to include files

--- patients/profile.php
+++ patients/profile.php
@@ -1,12 +1,5 @@
 <?php 
-
-session_start();
-require_once("connect.php");
-
-$username = $_SESSION["userid"];
-$user = $_SESSION["user"];
-$ID = $_SESSION["user"]["Patient_SSN"];
-
+include "../inc/session_header.php";
 
 //read the row of the selected client from the database table 
 $sql = $conn->prepare("SELECT * FROM patients WHERE `Patient_SSN` = ?");
