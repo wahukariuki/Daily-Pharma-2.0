f3df2d9561d2a18fdf664d4ed1759cf475f756f9
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2023-09-04 13:27:08 +0300

    Refactoring to incorporate footer files

--- admin/adminView.php
+++ admin/adminView.php
@@ -1,5 +1,5 @@
 <?php
-include "../inc/header.php";
+include "../inc/view_header.php";
 ?>
 
     <!-- Above fold -->
@@ -18,7 +18,8 @@ include "../inc/header.php";
 
 
     <div class="item"></div>
-        <div class="title-text">
+
+    <div class="title-text">
             <p>Users</p>
             <h1>Who uses our system?</h1>
         </div>
@@ -294,78 +295,8 @@ include "../inc/header.php";
             </nav>
         </div>
 
-
-
-
-    <!--Footer-->
-    <section id="footer">
-        <div class="title-text">
-            <p>CONTACT US</p>
-            <h1>Any Queries?</h1>
-        </div>
-
-        <div class="footer-row">
-            <div class="footer-left">
-                <h1>Contact information</h1>
-                <div class="contact-link">
-
-                    <div class="contact-info">
-                        <i class="uil uil-twitter"></i>
-                        <span>@DailyPharma</span>
-                    </div>
-
-                    <div class="contact-info">
-                        <i class="uil uil-instagram"></i>
-                        <span>@TheDailyPharma</span>
-                    </div>
-
-                    <div class="contact-info">
-                        <i class="uil uil-facebook"></i>
-                        <span>@DailyPharma</span>
-                    </div>
-
-                    <div class="contact-info">
-                        <i class="uil uil-linkedin"></i>
-                        <span>@DailyPharma - Medical Website</span>
-                    </div>
-
-                    <div class="contact-info">
-                        <i class="uil uil-at"></i>
-                        <span>DailyPharma@gmail.com</span>
-                    </div>
-
-                    <div class="contact-info">
-                        <i class="uil uil-calling"></i>
-                        <span>0769690000</span>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="footer-right">
-                <div class="quick-links">
-                    <h1>Quick Links</h1>
-                    <ul>
-                      <li><a href="index.html">Home</a></li>
-                      <li><a href="#service">About Us</a></li>
-                      <li><a href="#feature">Features</a></li>
-                      <li><a href="#">FAQ</a></li>
-                      <li><a href="#">Privacy Policy</a></li>
-                      <li><a href="#">Terms and Conditions</a></li>
-                    </ul>
-                  </div>
-            </div>
-        </div>
-
-        <div class="additional-info">
-            <p>&copy; 2023 DailyPharma. All rights reserved.</p>
-        </div>
-    </section>
-
-
     <script src="../overlay_script.js"></script>
     <script src="../image_slider.js"></script>
     <script src="../category_selector.js"></script>
-    
-</body>
-</html>
+
+<?php include "../inc/footer.php";?>
---
+++ inc/footer.php
@@ -0,0 +1,74 @@
+   <!--Footer-->
+   <section id="footer">
+        <div class="title-text">
+            <p>CONTACT US</p>
+            <h1>Any Queries?</h1>
+        </div>
+
+        <div class="footer-row">
+            <div class="footer-left">
+                <h1>Contact information</h1>
+                <div class="contact-link">
+
+                    <div class="contact-info">
+                        <i class="uil uil-twitter"></i>
+                        <span>@DailyPharma</span>
+                    </div>
+
+                    <div class="contact-info">
+                        <i class="uil uil-instagram"></i>
+                        <span>@TheDailyPharma</span>
+                    </div>
+
+                    <div class="contact-info">
+                        <i class="uil uil-facebook"></i>
+                        <span>@DailyPharma</span>
+                    </div>
+
+                    <div class="contact-info">
+                        <i class="uil uil-linkedin"></i>
+                        <span>@DailyPharma - Medical Website</span>
+                    </div>
+
+                    <div class="contact-info">
+                        <i class="uil uil-at"></i>
+                        <span>DailyPharma@gmail.com</span>
+                    </div>
+
+                    <div class="contact-info">
+                        <i class="uil uil-calling"></i>
+                        <span>0769690000</span>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="footer-right">
+                <div class="quick-links">
+                    <h1>Quick Links</h1>
+                    <ul>
+                      <li><a href="../index.html">Home</a></li>
+                      <li><a href="../index.html#service">About Us</a></li>
+                      <li><a href="../index.html#feature">Features</a></li>
+                      <li><a href="#">FAQ</a></li>
+                      <li><a href="#">Privacy Policy</a></li>
+                      <li><a href="#">Terms and Conditions</a></li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+
+        <div class="additional-info">
+            <p>&copy; 2023 DailyPharma. All rights reserved.</p>
+        </div>
+    </section>
+
+
+
+    
+</body>
+</html>
+
+<?php
+$conn->close();
+?>
