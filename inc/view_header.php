932465970a76d80589e3f5c44793fe65332a5bb4
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2023-09-12 19:52:04 +0300

    Project Assignment QN3

--- inc/view_header.php
+++ inc/view_header.php
@@ -24,7 +24,7 @@ $username = $_SESSION["username"];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../style.css">
     <title> DailyPharma - <?php echo $user?> Home</title>
