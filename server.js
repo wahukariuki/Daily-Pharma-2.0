94de7e999c686f5f7bcbc2f91be5012a0ab3a090
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2023-11-25 12:15:19 +0300

    Addition of access and refresh tokens

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.listen(3000 , ()=>{
     console.log("API up and running")
 })
 
-app.get('/users', (req,res)=>{
-    res.json(users)
+app.get('/users', authenticateToken ,(req,res)=>{
+    res.json(users.filter(user => user.name == req.user.name ))
 })
 
 app.post('/users', async (req,res)=>{
@@ -54,22 +54,48 @@ app.post('/users/login', async (req,res)=>{
     if (user == null) return res.sendStatus(400).send("Cannot find user")
     try{
         if(await bcrypt.compare(req.body.password, user.password)){
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-            // const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-            // refreshTokens.push(refreshToken)
-            res.json({accessToken : accessToken})
+
+            const accessToken = generateAccessToken(user)
+            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+            refreshTokens.push(refreshToken)
+            res.json({accessToken : accessToken, refreshToken : refreshToken})
+
         }else{
             res.send("NOT ALLOWED")
         }
     }catch{
         res.sendStatus(500).send()
-    }   
-    
-    //Creating webtoken
-    
+    }     
+})
+
+app.post('/token',(req,res)=>{
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(401)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err,user)=>{
+        if(err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({name : user.name})
+        res.json({accessToken : accessToken})
+
+    })
+})
+
+app.delete('/logout',(req,res)=>{
+    refreshTokens = refreshTokens.filter(token => token!=req.body.token)
+    res.sendStatus(204)
 })
 
 function authenticateToken(req,res,next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user)=>{
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+function generateAccessToken(user){
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1m'})
 }
