bbeeec92e92d6eecce21b66483b0935b10f6c9b7
torieeee <victoria.mwaura@strathmore.edu>
2023-11-27 21:12:27 +0300

    making api accessible to users

--- app/company/companyView.php
+++ app/company/companyView.php
@@ -111,6 +111,7 @@ include "../functions.php";
                             <a class='btn btn-danger btn-sm' href='confirmDeleteDrug.php?id=" . $row["Drug_ID"] . "'>Delete</a>
                         </td>
                     
+                        
                          </tr>";
                          }
                      } else {
